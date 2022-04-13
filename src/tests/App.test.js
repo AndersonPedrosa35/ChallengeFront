ce723288acf237b663fade7891d91c8aea23abdf
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2022-04-12 21:02:19 -0300

    Insert - Testing - Starting renders and interactions test in Pending Quotes Table

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -75,7 +75,7 @@ describe('Testing the container quotes', () => {
 });
 
 describe('test the interactions with inputs into create quotes form', () => {
-  it('should starting the interactions with inputs into create quotes form', () => {
+  it('test end-to-end into create quotes form', () => {
     const { getByTestId, getByRole } = renderWithRoute(<App testing={ true } />);
     const inputFrom = getByTestId('from').childNodes[1];
     const inputDestination = getByTestId('destination').childNodes[1];
@@ -102,5 +102,47 @@ describe('test the interactions with inputs into create quotes form', () => {
     expect(inputTransportation).toHaveValue('train');
     expect(inputName).toHaveValue('Juan');
     expect(buttonSubmit).toBeEnabled()
+
+    userEvent.click(buttonSubmit);
+    expect(inputFrom).toHaveValue('');
+    expect(inputDestination).toHaveValue('');
+    expect(inputDepartDate).toHaveValue('1');
+    expect(inputReturnDate).toHaveValue('1');
+    expect(inputPeople).toHaveValue('1');
+    expect(inputTransportation).toHaveValue('airplane');
+    expect(inputName).toHaveValue('');
   });
-})
+
+  it('should button into create quotes form is disabled', () => {
+    const { getByTestId, getByRole } = renderWithRoute(<App testing={ true } />);
+    const inputFrom = getByTestId('from').childNodes[1];
+    const inputDestination = getByTestId('destination').childNodes[1];
+    const inputDepartDate = getByTestId('departDate').childNodes[1];
+    const inputReturnDate = getByTestId('returnDate').childNodes[1];
+    const inputPeople = getByTestId('people').childNodes[1];
+    const inputTransportation = getByTestId('transportation').childNodes[1];
+    const inputName = getByTestId('name').childNodes[1];
+    const buttonSubmit = getByRole('button', { name: 'Create a quote' });
+
+    userEvent.type(inputFrom, 'Buenos Aires');
+    userEvent.type(inputDestination, 'Santiago');
+    userEvent.selectOptions(inputDepartDate, '2');
+    userEvent.selectOptions(inputReturnDate, '3');
+    userEvent.selectOptions(inputPeople, '2');
+    userEvent.selectOptions(inputTransportation, 'train');
+    userEvent.type(inputName, '');
+
+    expect(inputFrom).toHaveValue('Buenos Aires');
+    expect(inputDestination).toHaveValue('Santiago');
+    expect(inputDepartDate).toHaveValue('2');
+    expect(inputReturnDate).toHaveValue('3');
+    expect(inputPeople).toHaveValue('2');
+    expect(inputTransportation).toHaveValue('train');
+    expect(inputName).toHaveValue('');
+    expect(buttonSubmit).toBeDisabled()
+  });
+});
+
+describe('test the interactions and renders with the Pending Quotes Table', () => {
+  
+});
